206e55d9c51528e001d0a71feb7b10ab2b81cc13
dvub <dvubdevs@gmail.com>
2025-02-13 19:12:46 -0800

    fuh

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,22 @@ fn main() -> Result<(), ApplicationError> {
             Label::new(cx, "Vizia TODO")
                 .class("header")
                 .font_size(25)
-                .padding(Percentage(10.0));
-            Textbox::new(cx, AppData::value);
+                .padding_bottom(Pixels(20.0))
+                .alignment(Alignment::TopCenter);
+
+            HStack::new(cx, |cx| {
+                Label::new(cx, "Add a new item:").font_size(15);
+
+                Textbox::new(cx, AppData::value)
+                    .width(Pixels(100.0))
+                    .color(Color::black());
+                Button::new(cx, |cx| Label::new(cx, "Add")).background_color(Color::green());
+            })
+            .gap(Pixels(10.0));
+
+            Button::new(cx, |cx| Label::new(cx, "Add")).background_color(Color::green());
         })
-        .class("main-content")
-        .alignment(Alignment::TopCenter);
+        .class("main-content");
     })
     .title("Counter")
     .inner_size((800, 600))
