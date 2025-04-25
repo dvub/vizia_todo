8a73003e10ad3e4e5a121f68bddfa8ad9a5d11c8
dvub <dvubdevs@gmail.com>
2025-04-24 17:02:40 -0700

    actually render todos (in corner)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use vizia::prelude::*;
 
+#[derive(Debug, Clone)]
+struct Todo {
+    title: String,
+}
+impl Data for Todo {
+    // TODO: this needs to be improved at some point
+    fn same(&self, other: &Self) -> bool {
+        self.title == other.title
+    }
+}
+
 #[derive(Lens)]
 struct AppData {
-    value: String,
+    todos: Vec<Todo>,
+    current_data: String,
 }
-impl Model for AppData {}
+impl Model for AppData {
+    fn event(&mut self, _: &mut EventContext, event: &mut Event) {
+        event.map(|event, _| match event {
+            AppEvent::AddTodo => {
+                self.todos.push(Todo {
+                    title: self.current_data.clone(),
+                });
+                println!("{:?}", self.todos)
+            }
+            AppEvent::SetInputValue(value) => self.current_data = value.to_string(),
+        });
+    }
+}
+
+pub enum AppEvent {
+    AddTodo,
+    SetInputValue(String),
+}
 
 fn main() -> Result<(), ApplicationError> {
     Application::new(|cx| {
         AppData {
-            value: String::new(),
+            todos: Vec::new(),
+            current_data: "Hello, world".to_owned(),
         }
         .build(cx);
 
@@ -26,14 +56,22 @@ fn main() -> Result<(), ApplicationError> {
             HStack::new(cx, |cx| {
                 Label::new(cx, "Add a new item:").font_size(15);
 
-                Textbox::new(cx, AppData::value)
+                Textbox::new(cx, AppData::current_data)
                     .width(Pixels(100.0))
-                    .color(Color::black());
-                Button::new(cx, |cx| Label::new(cx, "Add")).background_color(Color::green());
+                    .color(Color::black())
+                    .on_submit(|cx, value, _| cx.emit(AppEvent::SetInputValue(value)));
+                Button::new(cx, |cx| Label::new(cx, "Add"))
+                    .background_color(Color::green())
+                    .on_press(|cx| cx.emit(AppEvent::AddTodo));
             })
             .gap(Pixels(10.0));
 
-            Button::new(cx, |cx| Label::new(cx, "Add")).background_color(Color::green());
+            Binding::new(cx, AppData::todos, |cx, s| {
+                s.get(cx).iter().for_each(|x| {
+                    // println!("{}", x.title.clone());
+                    Label::new(cx, x.title.clone());
+                });
+            });
         })
         .class("main-content");
     })
